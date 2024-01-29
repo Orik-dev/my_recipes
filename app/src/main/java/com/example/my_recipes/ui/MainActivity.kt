131bf369c39b1445e25fe2b5ea4fbab4fae94df4
orxan <orxan4ik7777@gmail.com>
2024-01-29 15:12:06 +0400

    Recipes

---
+++ app/src/main/java/com/example/my_recipes/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.my_recipes.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.my_recipes.R
+import com.example.my_recipes.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+private lateinit var navController: NavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/my_recipes/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.my_recipes.util
+
+
+    class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "9b782ad568274184b077d9245b044a75"
+        }
+    }
+
