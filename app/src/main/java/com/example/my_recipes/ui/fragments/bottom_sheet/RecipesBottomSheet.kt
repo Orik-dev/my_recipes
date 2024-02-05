0332bf7f93afd4ad64e07677f268c49c2b7f9a10
orxan <orxan4ik7777@gmail.com>
2024-02-05 16:03:01 +0400

    RecipesBottomSheet - Persist Selected

--- app/src/main/java/com/example/my_recipes/ui/fragments/bottom_sheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/example/my_recipes/ui/fragments/bottom_sheet/RecipesBottomSheet.kt
@@ -1,21 +1,99 @@
 package com.example.my_recipes.ui.fragments.bottom_sheet
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.example.my_recipes.R
+import com.example.my_recipes.databinding.RecipesBottomSheetBinding
+import com.example.my_recipes.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.my_recipes.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.my_recipes.viewmodels.RecipesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.lang.Exception
+import java.util.Locale
 
 
 class RecipesBottomSheet : BottomSheetDialogFragment() {
 
+    private lateinit var recipesViewModel: RecipesViewModel
+
+    private var mealTypeChip = DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
+    private var _binding: RecipesBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
-        return view
+    ): View {
+        // Используем ViewBinding для надувания макета
+        _binding = RecipesBottomSheetBinding.inflate(inflater, container, false)
+
+
+        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner) { value ->
+            mealTypeChip = value.selectedMealType
+            dietTypeChip = value.selectedDietType
+
+            updateChip(value.selectedMealTypeId, binding.mealTypeChipGroup)
+            updateChip(value.selectedDietTypeId, binding.dietTypeChipGroup)
+        }
+
+
+        binding.mealTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedMealType = chip.text.toString().lowercase(Locale.ROOT)
+
+            mealTypeChip = selectedMealType
+            mealTypeChipId = selectedChipId
+
+
+            binding.dietTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+                val chip = group.findViewById<Chip>(selectedChipId)
+                val selectedDietType = chip.text.toString().lowercase(Locale.ROOT)
+
+                dietTypeChip = selectedDietType
+                dietTypeChipId = selectedChipId
+            }
+
+            binding.applyBtn.setOnClickListener {
+                recipesViewModel.saveMealAndDietType(
+                    mealTypeChip,
+                    mealTypeChipId,
+                    dietTypeChip,
+                    dietTypeChipId
+                )
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+                chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                Log.d("RecipesBottomSheet", e.message.toString())
+            }
+        }
     }
 }
