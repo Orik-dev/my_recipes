eee9818e3554bfbd9db5527aae1abd9f1ba08c5f
orxan <orxan4ik7777@gmail.com>
2024-02-05 15:01:14 +0400

    dataStore inject recipesViewModel

--- app/src/main/java/com/example/my_recipes/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/my_recipes/viewmodels/RecipesViewModel.kt
@@ -1,19 +1,52 @@
 package com.example.my_recipes.viewmodels
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.my_recipes.data.DataStoreRepository
 import com.example.my_recipes.util.Constants
+import com.example.my_recipes.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.my_recipes.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.my_recipes.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.example.my_recipes.util.Constants.Companion.QUERY_API_KEY
 import com.example.my_recipes.util.Constants.Companion.QUERY_DIET
 import com.example.my_recipes.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.example.my_recipes.util.Constants.Companion.QUERY_NUMBER
 import com.example.my_recipes.util.Constants.Companion.QUERY_TYPE
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+@HiltViewModel
+class RecipesViewModel @Inject constructor(
+    @ApplicationContext  private val context: Context,
+    private val dataStoreRepository: DataStoreRepository
+) : ViewModel() {
+
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int,dietType: String, dietTypeId:Int){
+        viewModelScope.launch (Dispatchers.IO){
+            dataStoreRepository.saveMealAndDietType(mealType,mealTypeId,dietType,dietTypeId)
+        }
+    }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
 
 
         queries[QUERY_NUMBER] = "50"
